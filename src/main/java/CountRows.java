a4f8234bc042edb784b296ca2a78247b3d59d4df
tranphuquycubin <131520832+tranphuquycubin@users.noreply.github.com>
2024-10-23 12:45:42 +0700

    pom.xml

---
+++ src/main/java/CountRows.java
@@ -0,0 +1,36 @@
+
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author Admin
+ */
+@WebServlet(urlPatterns = {"/count_rows"})
+public class CountRows extends HttpServlet {
+
+    private static final long serialVersionUID = 1L;
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+        BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(resp.getOutputStream(), "UTF-8"));
+
+        resp.setContentType("text/plain");
+        resp.setStatus(200);
+        writer.write("100");
+        writer.flush();
+        writer.close();
+    }
+}
---
+++ src/main/webapp/cloud24.js
@@ -0,0 +1,11 @@
+/* 
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/JSP_Servlet/JavaScript.js to edit this template
+ */
+console.log("Hello");
+let clickButton=document.getElementById("clickButton");
+clickButton.addEventListener("click",function (){
+    alert("Hi");
+    clickButton.style.backgroundColor = "#7FFF00";
+});
+

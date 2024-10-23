c601df960bc3be76049ffacc5c1ef3bcc4ac3489
tranphuquycubin <131520832+tranphuquycubin@users.noreply.github.com>
2024-10-23 15:16:15 +0700

    update3

--- src/main/java/TimeServlet.java
+++ src/main/java/TimeServlet.java
@@ -11,7 +11,7 @@ import javax.servlet.http.HttpServletResponse;
 @WebServlet(urlPatterns = {"/time"})
 public class TimeServlet extends HttpServlet {
 
-    private static final long serialVersionUID = 1L;
+    private static final long serialVersionUID = 1L;  
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {

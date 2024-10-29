64edce3b114b6223793b116a45b8c09680dbcd70
tranphuquycubin <131520832+tranphuquycubin@users.noreply.github.com>
2024-10-29 10:25:29 +0700

    working on it

---
+++ src/main/java/Configuration.java
@@ -0,0 +1,47 @@
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author Admin
+ */
+public class Configuration {
+      private static Connection mySQLClient = null;
+
+    private final static String JDBC_URL = "jdbc:mysql://localhost:3306/Cloud24";
+    private final static String DB_USER = "cloud24";
+    private final static String DB_PASSWORD = "cloud24";
+   
+
+    protected Configuration() {
+    };
+
+    public static Connection getDBConnection() throws Exception {
+        if (!isConnectionValid(mySQLClient)) {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            mySQLClient = DriverManager.getConnection(JDBC_URL, DB_USER, DB_PASSWORD);
+        }
+        return mySQLClient;
+    }
+
+    public static boolean isConnectionValid(Connection connection) {
+        try {
+            if (connection != null && !connection.isClosed()) {
+                // Running a simple validation query
+                connection.prepareStatement("SELECT 1");
+                return true;
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return false;
+    }
+
+}
---
+++ src/main/java/CountAPI.java
@@ -0,0 +1,60 @@
+
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author Admin
+ */
+@WebServlet(urlPatterns = {"/count_roles"})
+public class CountAPI extends HttpServlet {
+
+    private static final long serialVersionUID = 1L;
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+
+            BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(resp.getOutputStream(), "UTF-8"));
+            
+            try{
+            Connection con = Configuration.getDBConnection();
+            
+            
+            PreparedStatement st = con.prepareStatement("SELECT COUNT(ID) as count FROM Role");
+            ResultSet rs = st.executeQuery();
+
+            int count = rs.getInt("count");
+            
+            
+            resp.setContentType("text/plain");
+            resp.setStatus(200);
+            writer.write(Integer.toString(count));
+            writer.flush();
+            writer.close();
+        } catch (Exception ex) {
+            //Logger.getLogger(CountAPI.class.getName()).log(Level.SEVERE, null, ex);
+             resp.setContentType("text/plain");
+            resp.setStatus(500);
+            writer.write(ex.toString());
+            writer.flush();
+            writer.close();
+        }
+    }
+}

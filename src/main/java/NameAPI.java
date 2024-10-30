7fa88ec90b85433b873d7adbcfbb2e490ff4420a
tranphuquycubin <131520832+tranphuquycubin@users.noreply.github.com>
2024-10-30 08:39:07 +0700

    working on it

--- src/main/java/NameAPI.java
+++ src/main/java/NameAPI.java
@@ -20,7 +20,7 @@ import javax.servlet.http.HttpServletResponse;
  *
  * @author Admin
  */
-@WebServlet(urlPatterns = {"/nameapi"})
+@WebServlet(urlPatterns = {"/name_api"})
 public class NameAPI extends HttpServlet {
 
     private static final long serialVersionUID = 1L;
